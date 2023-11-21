f907f343619bda043df67130efc25264a7aecc0f
Junaid Ali <junaid@innvotech.io>
2023-11-21 09:11:19 +0500

    sql fix

--- SQL File/HYIP MAX.sql
+++ SQL File/HYIP MAX.sql
@@ -1285,7 +1285,7 @@ CREATE TABLE `user_interests` (
   `purpouse` varchar(255) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
