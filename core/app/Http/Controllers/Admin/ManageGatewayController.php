f027baf3645c85dbb03bf2df3c08a2bc5699d362
Junaid Ali <junaid@innvotech.io>
2023-11-21 19:37:11 +0500

    New small fix

--- core/app/Http/Controllers/Admin/ManageGatewayController.php
+++ core/app/Http/Controllers/Admin/ManageGatewayController.php
@@ -1384,7 +1384,6 @@ class ManageGatewayController extends Controller
             refferMoney($booking->user_id, $booking->user->refferedBy, 'invest', $booking->amount,$type);
 
         }
-        dd("hi");
 
         if ($booking->wallet_type == "business_value_payments") {
 
